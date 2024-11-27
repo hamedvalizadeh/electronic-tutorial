b21df5cbe42c8ec079e6fd36870d9b8f8fa53ff7
valizadehh <valizadehh@LT1352>
2024-11-27 13:53:10 +0330

    create project 19 for AVR

--- Embedded_Programming/avr-workshop-a-hands-on-introduction-with-60-projects/Code/19_USART_TMP36_ATmega328p/main.c
+++ Embedded_Programming/avr-workshop-a-hands-on-introduction-with-60-projects/Code/19_USART_TMP36_ATmega328p/main.c
@@ -1,5 +1,3 @@
-// Project 19 – Digital thermometer with terminal output
-
 #include <avr/io.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,36 +22,77 @@ void setup_adc()
 
 uint16_t read_adc()
 {
-    // start ADC
     ADCSRA |= (1 << ADSC);
     while (ADCSRA & (1 << ADSC))
     {
-    }
+    };
     _delay_ms(5);
     return ADC;
 }
 
-uint8_t convert_adc_to_temprature_celcius(uint16_t adc_value)
+float adc_to_temp_celcius(uint16_t adc_val)
 {
-    float voltage = (adc_value * 5);
-    voltage = voltage / 1024;
-    voltage = voltage - 0.5;
-    voltage = voltage * 100;
-    return (uint8_t)round(voltage);
+    float volt = (adc_val * 5);
+    volt = volt / 1024;
+    float temp = ((volt - 0.5) * 100);
+    return temp;
+}
+
+void USARTInit(void)
+{
+    // Set baud rate registers
+    UBRR0H = (uint8_t)(UBRR_VALUE >> 8);
+    UBRR0L = (uint8_t)UBRR_VALUE;
+
+    // Set data frame format to 8 data bits, no parity, 1 stop bit
+    UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);
+
+    // enable transmission and reception
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+}
+
+void USARTSendByte(unsigned char u8Data) // send a byte to USART
+{
+    // wait while previous byte is sent
+    while (!(UCSR0A & (1 << UDRE0)))
+    {
+    };
+    // Transmit data
+    UDR0 = u8Data;
+}
+
+void sendString(char myString[])
+{
+    uint8_t a = 0;
+    while (myString[a])
+    {
+        USARTSendByte(myString[a]);
+        a++;
+    }
 }
 
 int main(void)
 {
-    uint16_t adc_value;
-    uint8_t temp_value;
-    DDRC = 0b00000000;
+    float temp;
 
+    char temp_string[10] = "";
+    char temp_label[14] = "Temperature: ";
+    char unit_lable[14] = " degrees C ";
+    char newline[4] = "\r\n";
+
+    DDRC = 0b00000000;
+    USARTInit();
     setup_adc();
 
     while (1)
     {
-        adc_value = read_adc();
-        temp_value = convert_adc_to_temprature_celcius(adc_value);
+        temp = adc_to_temp_celcius(read_adc());
+
+        sendString(temp_label);
+        dtostrf(temp, 6, 2, temp_string);
+        sendString(temp_string);
+        sendString(unit_lable);
+        sendString(newline);
         _delay_ms(1000);
     }
 
