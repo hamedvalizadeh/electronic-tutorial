716c9d35e9617edc95e419da5abc25e54ffb2ed1
Hamed Valizadeh Kelasar <valizadehh@aryasasol.com>
2025-05-10 10:48:48 +0330

    project 47: reset button

---
+++ Embedded_Programming/avr-workshop-a-hands-on-introduction-with-60-projects/Code/47_Reset_ATTiny85/main.c
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRB |= (1 << PB0) | (1 << PB1);
+
+    PORTB |= (1 << PB0);
+    _delay_ms(1000);
+    PORTB &= ~(1 << PB0);
+    while (1)
+    {
+        PORTB ^= (1 << PB1);
+        _delay_ms(200);
+    }
+    return 0;
+}
