53a418e1d8fdcb8184cccb67e9fd6ae0732b136c
Hamed Valizadeh Kelasar <valizadehh@aryasasol.com>
2025-09-15 19:23:53 +0330

    prj 54: edit ADC lib to support all AVR family

--- Embedded_Programming/avr-workshop-a-hands-on-introduction-with-60-projects/Code/54_TMP36_DS3232_ATMega328p/lib/adc.c
+++ Embedded_Programming/avr-workshop-a-hands-on-introduction-with-60-projects/Code/54_TMP36_DS3232_ATMega328p/lib/adc.c
@@ -1,39 +1,68 @@
 #include "adc.h"
 #include <util/delay.h>
 
+// Detect number of ADC channels based on device
+#if defined(ADC)
+#define ADC_MAX_CHANNELS 16
+#elif defined(ADC0)
+#define ADC_MAX_CHANNELS 8
+#else
+#define ADC_MAX_CHANNELS 4
+#endif
+
 void adc_init(adc_ref_t ref, adc_prescaler_t prescaler)
 {
-    // Clear reference selection bits
+#if defined(ADMUX)
+    // Clear reference selection bits if present
+#if defined(REFS1) && defined(REFS0)
     ADMUX &= ~((1 << REFS1) | (1 << REFS0));
-    ADMUX |= (ref << REFS0); // Set reference
+    ADMUX |= (ref << REFS0);
+#endif
+#endif
 
+#if defined(ADCSRA)
     // Enable ADC
     ADCSRA |= (1 << ADEN);
 
-    // Set prescaler (division factor for ADC clock)
+    // Set prescaler if available
+#if defined(ADPS2)
     ADCSRA &= ~((1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0));
     ADCSRA |= prescaler;
+#endif
+#endif
 }
 
 void adc_select_channel(uint8_t channel)
 {
-    if (channel > 7)
-        return; // Only ADC0–ADC7 on ATmega328P
+    if (channel >= ADC_MAX_CHANNELS)
+        return; // prevent out-of-bounds
 
-    // Clear previous channel selection
-    ADMUX &= 0xF0; // Keep reference bits, clear MUX[3:0]
-    ADMUX |= channel;
+#if defined(ADMUX)
+#if defined(MUX0)
+    // Clear previous channel bits
+    ADMUX &= 0xF0;
+    ADMUX |= channel & 0x0F;
+#endif
+#endif
 }
 
 uint16_t adc_read(void)
 {
+#if defined(ADCSRA) && defined(ADSC)
     // Start conversion
     ADCSRA |= (1 << ADSC);
 
     // Wait for conversion to finish
     while (ADCSRA & (1 << ADSC))
         ;
-    _delay_ms(5);
-    // Return ADC value (10-bit)
+
+#if defined(ADC)
     return ADC;
+#else
+    return 0;
+#endif
+
+#else
+    return 0;
+#endif
 }
--- Embedded_Programming/avr-workshop-a-hands-on-introduction-with-60-projects/Code/54_TMP36_DS3232_ATMega328p/lib/adc.h
+++ Embedded_Programming/avr-workshop-a-hands-on-introduction-with-60-projects/Code/54_TMP36_DS3232_ATMega328p/lib/adc.h
@@ -2,20 +2,24 @@
 #define ADC_H
 
 #include <avr/io.h>
-#include <util/delay.h>
 #include <stdint.h>
 
 // ADC reference voltage options
 typedef enum
 {
+#if defined(REFS1) && defined(REFS0)
     ADC_REF_AREF = 0,
     ADC_REF_AVCC = 1,
-    ADC_REF_INTERNAL = 3 // 1.1V internal reference
+    ADC_REF_INTERNAL = 3
+#else
+    ADC_REF_DEFAULT = 0
+#endif
 } adc_ref_t;
 
 // ADC prescaler options
 typedef enum
 {
+#if defined(ADPS2)
     ADC_PRESCALER_2 = 1,
     ADC_PRESCALER_4 = 2,
     ADC_PRESCALER_8 = 3,
@@ -23,12 +27,15 @@ typedef enum
     ADC_PRESCALER_32 = 5,
     ADC_PRESCALER_64 = 6,
     ADC_PRESCALER_128 = 7
+#else
+    ADC_PRESCALER_DEFAULT = 0
+#endif
 } adc_prescaler_t;
 
-// Initialize ADC with reference and prescaler
+// Initialize ADC
 void adc_init(adc_ref_t ref, adc_prescaler_t prescaler);
 
-// Select ADC channel (0–7)
+// Select ADC channel (0..MAX)
 void adc_select_channel(uint8_t channel);
 
 // Read ADC value (blocking)
